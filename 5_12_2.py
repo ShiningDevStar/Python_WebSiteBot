3edd38005c3d7b348d81202d2e4015e182701f28
ShiningStar30320 <ShiningStar30320@gmail.com>
2022-05-12 11:02:22 -0700

    AutoRegisterAccount,VerifyAccount,AutoLogin
    Classification and Test

--- 5_12_2.py
+++ 5_12_2.py
@@ -1,6 +1,7 @@
 import outlook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import selenium.webdriver.support.expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -15,14 +16,16 @@ import time
 from anycaptcha import AnycaptchaClient, HCaptchaTaskProxyless, RecaptchaV2TaskProxyless, RecaptchaV3TaskProxyless, \
     ImageToTextTask ,RecaptchaV2Task ,HCaptchaTask , FunCaptchaProxylessTask,ZaloTask
 import random
-
+import sys
 
 
 class AutoRegisterAccount():
-    def __init__(self):
+    def __init__(self,emailaddr,emailpassword):
         self.driver = None
         self.chrome_options = Options()
         self.exe_path = "drivers/chromedriver"
+        self.username = emailaddr
+        self.password = emailpassword
         pass
     def demo_imagetotext(self,imgpath):
         api_key = '89fd718af6f547dcb416f6e06a7ad532'
@@ -47,13 +50,13 @@ class AutoRegisterAccount():
     def page_has_loaded(self):
         page_state = self.driver.execute_script('return document.readyState;')
         return True
-    def AccountRegite(self,name,emailaddr,phonenumber):
+    def AccountRegist(self,name,phonenumber):
         self.driver = webdriver.Chrome(chrome_options=self.chrome_options, executable_path=self.exe_path)
         
         self.driver.get('https://algeria.blsspainvisa.com/')
         if self.page_has_loaded() is True:
             pass
-        
+
         self.driver.find_element(By.XPATH,"//div[@class='popupCloseIcon']").click()
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/header[1]/div[2]/nav[1]/a[3]").click();
         self.driver.implicitly_wait(2);
@@ -64,7 +67,7 @@ class AutoRegisterAccount():
         self.driver.implicitly_wait(2);
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[2]/div[1]/a[1]").click()
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[1]/div[2]/input[1]").send_keys(name)
-        self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[2]/div[2]/input[1]").send_keys(emailaddr)
+        self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[2]/div[2]/input[1]").send_keys(self.username)
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[3]/div[2]/div[2]/input[1]").send_keys(phonenumber)
         location = self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[4]/div[2]/select[1]")
         location.click()
@@ -76,23 +79,25 @@ class AutoRegisterAccount():
         img_url = img_captcha.get_attribute("src")
         with open(f"temp.png","wb") as file:
             file.write(data_captcha)
-        code = demo_imagetotext("temp.png")
+        code = self.demo_imagetotext("temp.png")
         print(code)
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[6]/div[2]/input[1]").send_keys(code)
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/form[1]/div[7]/input[1]").click()
         print("Complete")
-    def AccountVerify(self,emailaddr,password):
+    def AccountVerify(self):
         try:
             mail = outlook.Outlook()
-            mail.login(mailAddr,mailPass)
+            mail.login(self.username,self.password)
             mail.inbox()
-            mailText = mail.mailbody()         
+            mail.read()
+            mailText = mail.mailbody()
+            print(mailText)
             mail.logout()
             self.userInfo(mailText)
             self.verify(mailText)
         except Exception as e:
             print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
-    def userInfo(mailbody):
+    def userInfo(self,mailbody):
         try:
             index = mailbody.find("Username:")
             indexend = mailbody[index:].find('</')
@@ -102,7 +107,7 @@ class AutoRegisterAccount():
             print(mailbody[index+18:index+indexend])
         except Exception as e:
             print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
-    def verify(mailbody):
+    def verify(self,mailbody):
         index = mailbody.find("href=")
         indexend = mailbody[index:].find('>')
         verifyurl = mailbody[index+6:index+indexend-1]
@@ -113,14 +118,14 @@ class AutoRegisterAccount():
         sleep(5)
     
 class AutoLogIn():
-    def __init__(self):
+    def __init__(self,emailaddr,emailpassword):
         self.driver = None
         self.chrome_options = Options()
         self.exe_path = "drivers/chromedriver"
-        self.username = "asdf@asdf.com"
-        self.password = "asdfsadf"
+        self.username = emailaddr
+        self.password = emailpassword
         pass
-    def LogIn(self,emailaddr,emailpassword):
+    def LogIn(self):
         self.driver = webdriver.Chrome(chrome_options=self.chrome_options, executable_path=self.exe_path)
         self.driver.get('https://algeria.blsspainvisa.com/')
         self.driver.find_element(By.XPATH,"//div[@class='popupCloseIcon']").click()
@@ -135,29 +140,28 @@ class AutoLogIn():
         print(self.driver.current_url)
         self.driver.implicitly_wait(2);
 
-        #MailAddr = input("MailAddress")
-        MailAddr = 'devtest0316@outlook.com'
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/input[1]").click();
-        self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/input[1]").send_keys(emailaddr);
+        self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/input[1]").send_keys(self.username);
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/input[2]").click();
         sleep(2);
         self.driver.implicitly_wait(2);
-        OTPcode = self.get_otp_from_email(emailaddr,emailpassword)
+        OTPcode = self.get_otp_from_email()
         #OTPcode=input("Please Input OTP code");
 
         #self.driver.find_element(By.XPATH,"").click();
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/input[1]").send_keys(OTPcode)
-        self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[2]/input[1]").send_keys(emailpassword)
+        self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[2]/input[1]").send_keys(self.password)
         self.driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/input[2]").click();
 
         self.check_isOpen()
-    def get_otp_from_email(self,mailAddr,mailPass):
+    def get_otp_from_email(self):
             mail = outlook.Outlook()
-            mail.login(mailAddr,mailPass)
+            mail.login(self.username,self.password)
             mail.inbox()
+            mail.read()
             mailText = mail.mailbody()
             mail.logout()
-            return self.find_opt(mailText)
+            return self.find_otp(mailText)
     def find_otp(self,mailbody):
             index = mailbody.find("OTP - ")
             print(mailbody[index:])
@@ -181,6 +185,9 @@ class AutoLogIn():
         )        
 if __name__ == "__main__":
     threads = []
-    logBot = AutoLogIn()
+    #registerBot = AutoRegisterAccount('devtest0317@outlook.com', 'slwgidaik1')
+    #registerBot.AccountRegist("John","542136874")
+    #registerBot.AccountVerify()
+    logBot = AutoLogIn('devtest0317@outlook.com','slwgidaik1')
     print("Outlook Creator ")
-    logBot.LogIn('devtest0316@outlook.com','xf21?lb6')
+    logBot.LogIn()
